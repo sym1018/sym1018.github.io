a406ea1ffc6f07a03bbad03058f9396f9c34c1ca
sym1018 <symzjs@outlook.com>
2026-03-23 17:46:57 +0800

    Site updated: 2026-03-23 17:46:56

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Remove fa-shake class from announcement icon to stop flashing
-  document.querySelectorAll('.fa-shake').forEach(function (el) {
-    el.classList.remove('fa-shake');
+  // Remove announcement bullhorn icon
+  document.querySelectorAll('#card-announcement .fa-bullhorn').forEach(function (el) {
+    el.remove();
   });
 });
